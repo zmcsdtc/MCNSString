d7353c5f269ba3a7b26f9f75bbd5deb89866489c
zmc <343512837@qq.com>
2016-01-15 18:20:48 +0800

    字符串的扩展方法

---
+++ MCNSString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCNSString
+//
+//  Created by ZMC on 16/1/15.
+//  Copyright © 2016年 Zmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
